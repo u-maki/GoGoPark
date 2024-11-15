9495b1c51b6091fa87097518890e28008f8e9f92
u-maki <maki0825.w@gmail.com>
2024-11-15 16:03:38 +0900

    ユーザー登録機能

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
+<<<<<<< Updated upstream
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+=======
+  devise_for :users
+  get 'maps/index'
+  root to: "maps#index"
+>>>>>>> Stashed changes
 end
