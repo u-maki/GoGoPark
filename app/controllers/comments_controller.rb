a6fd34314903cb80941e8d3c97ea1305a9d71d3a
u-maki <maki0825.w@gmail.com>
2024-11-20 11:32:42 +0900

    コメント投稿ページへの遷移

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,9 +1,6 @@
 class CommentsController < ApplicationController
   def new
-    if params[:park_id]
-      @park = Park.find(params[:park_id])
-      @comment = @park.comments.build
-    elsif params[:place_id]
+    if params[:place_id]
       @place_id = params[:place_id]
       @comment = Comment.new(google_place_id: @place_id)
     else
@@ -13,39 +10,27 @@ class CommentsController < ApplicationController
   end
 
   def create
-    if params[:park_id]
-      @park = Park.find(params[:park_id])
-      @comment = @park.comments.build(comment_params)
-      @comment.user = current_user
+    @comment = Comment.new(comment_params)
+    @comment.user = current_user
 
-      if @comment.save
-        flash[:notice] = "コメントを投稿しました。"
-        redirect_to park_path(@park)
-      else
-        flash[:alert] = "コメントの投稿に失敗しました。"
-        render :new
-      end
-    elsif params[:place_id]
-      @comment = Comment.new(comment_params)
-      @comment.user = current_user
+    if params[:place_id]
       @comment.google_place_id = params[:place_id]
-
-      if @comment.save
-        flash[:notice] = "コメントを投稿しました。"
-        redirect_to google_park_path(place_id: params[:place_id])
-      else
-        flash[:alert] = "コメントの投稿に失敗しました。"
-        render :new
-      end
     else
-      flash[:alert] = "コメントを投稿できませんでした。"
-      redirect_to root_path
+      @comment.park_id = params[:park_id]
+    end
+
+    if @comment.save
+      flash[:notice] = "コメントを投稿しました。"
+      redirect_to params[:place_id] ? google_park_path(place_id: params[:place_id]) : park_path(@comment.park_id)
+    else
+      flash[:alert] = "コメントの投稿に失敗しました。"
+      render :new
     end
   end
 
   private
 
   def comment_params
-    params.require(:comment).permit(:content)
+    params.require(:comment).permit(:content, :toilet, :diaper_changing_station, :vending_machine, :shop, :parking, :slide, :swing, photos: [])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,12 +6,14 @@ Rails.application.routes.draw do
   resources :users, only: [:edit, :update]
 
   # 公園リソース
+  get 'parks/details/:place_id', to: 'parks#show', as: :google_park
   resources :parks do
     # 公園に対するコメント機能
-    resources :comments, only: [:create,:new]
-    resources :parks, only: [:show]
-    
+    resources :comments, only: [:create, :new] # 必要なアクションのみ定義
   end
-  get 'parks/details/:place_id', to: 'parks#show', as: :google_park
+
+  # Google Place ID を利用したコメント機能
   get 'comments/new', to: 'comments#new', as: :new_comment
+  post 'comments', to: 'comments#create', as: :create_comment # 独立したコメント作成ルート
 end
+
