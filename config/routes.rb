27594cf857f75f7ad7c20c6aed9dd000042fd53c
u-maki <maki0825.w@gmail.com>
2024-11-27 15:08:49 +0900

    修正

--- config/routes.rb
+++ config/routes.rb
@@ -6,15 +6,13 @@ Rails.application.routes.draw do
   resources :users, only: [:edit, :update]
 
   # 公園リソース
-  get 'parks/details/:place_id', to: 'parks#show', as: :google_park
-  resources :parks do
+  resources :parks, only: [:show] do
     collection do
       get 'details/:place_id', to: 'parks#show', as: :google_park
     end
     resources :comments, only: [:create, :new, :index, :edit, :destroy, :update]
   end
-  
 
-  # コメントの独立した新規作成と作成ルート（Google Place ID 対応）
+  # 独立したコメント作成ルート（Google Place ID 対応）
   resources :comments, only: [:new, :create]
 end
